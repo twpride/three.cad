8a37572dcc74bc8999231b767cea9404db2bf5ff
howard <twpride@gmail.com>
2021-03-28 19:23:24 -0700

    merge redux back to index

--- src/index.js
+++ src/index.js
@@ -3,6 +3,27 @@ import ReactDOM from 'react-dom'
 import React from 'react'
 import { Root } from './app.jsx'
 
+
+import { createStore, applyMiddleware } from 'redux'
+import logger from 'redux-logger'
+
+
+
+function reducer(state = {}, action) {
+  switch (action.type) {
+    case 'toggle':
+      return { ...state, toggle: action.payload }
+    case 'rx-new-sketch':
+      return { ...state, sketches: [...state.sketches, action.idx] }
+    default:
+      return state
+  }
+}
+
+window.store = createStore(reducer, {sketches:[]}, applyMiddleware(logger))
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     React.createElement(Root, { store: store }, null)
