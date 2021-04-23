4e68e61b674359995b74924d9fbb2727ee4b97ea
howard <twpride@gmail.com>
2021-04-23 00:01:07 -0700

    test

--- extlib/fs/fs-access/file-save.mjs
+++ extlib/fs/fs-access/file-save.mjs
@@ -26,6 +26,7 @@ export default async (
   throwIfExistingHandleNotGood = false
 ) => {
   options.fileName = options.fileName || 'Untitled';
+  console.log(options.fileName,"ddddddddddd")
   const accept = {};
   if (options.mimeTypes) {
     options.mimeTypes.push(blob.type);
